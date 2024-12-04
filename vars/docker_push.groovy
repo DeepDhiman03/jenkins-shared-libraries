2ed0c2349250e161c426006bfe8f74fd58b5a935
DeepDhiman03 <118919161+DeepDhiman03@users.noreply.github.com>
2024-12-05 01:15:46 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,5 +1,6 @@
 def call(String Project, String ImageTag, String dockerhubuser){
- 
-  sh "docker login -u deepdhiman -p dckr_pat_MaJ5IMXfGB2AbwaMR7sc2F81klg"
+  withCredentials([usernamePassword(credentialsId: 'dockerHubCred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
+      sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
+  }
   sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
 }
